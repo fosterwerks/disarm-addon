96aac2d0b5a123d668b5602c75b2c7462091740b
Craig F Houghton <fosterwerks.cfh@outlook.com>
2019-12-08 11:31:50 -0600

    Moved menu stuff into main file

--- Disarm.lua
+++ Disarm.lua
@@ -1,5 +1,6 @@
 -- DECLARATIONS -----------------------------------------------------------------------------------
-if not Disarm then Disarm = {} end      -- Create namespace if necessary
+
+Disarm = {}
 
 Disarm.name = "Disarm"
 Disarm.version = 3
@@ -8,6 +9,10 @@ Disarm.Default = {
     Indicator.Position.X = 25,
     Indicator.Position.Y = 25
 }
+
+-- LIBRARIES --------------------------------------------------------------------------------------
+
+local LAM2 = LibraryAddonMenu2
 
 -- INITIALIZATION ---------------------------------------------------------------------------------
 
@@ -44,6 +49,52 @@ function Disarm:RestoreIndicatorPosition()
         DisarmIndicator:ClearAnchors()
         DisarmIndicator:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, left, top)
     end
+end
+
+-- SETTINGS MENU ----------------------------------------------------------------------------------
+
+function Disarm.CreateSettingsWindow()
+    local panelData = {
+		type = "panel",
+		name = "Disarm",
+		displayName = "Disarm",
+		author = "Fosterwerks",
+		version = Disarm.version,
+		slashCommand = "/disarm",
+		registerForRefresh = true,
+		registerForDefaults = true,
+    }
+    
+    local cntrlOptionsPanel = LAM2:RegisterAddonPanel("Disarm", panelData)
+
+    local optionsData = {
+        [1] = {
+			type = "header",
+			name = "Warning Indicator Settings"
+		},
+
+		[2] = {
+			type = "description",
+			text = "Here you can adjust how the warning indicator works."
+		},
+
+		[3] = {
+			type = "checkbox",
+			name = "Show Warning Indicator on Unequip",
+			tooltip = "Check this box if you want a persistent warning indicator to display when you've unequipped via the addon.",
+			default = true,
+			getFunc = function() return Disarm.savedVariables.Indicator.visible end,
+			setFunc = function(newValue)
+				Disarm.savedVariables.Indicator.visible = newValue end
+        },
+        
+        [4] = {
+			type = "description",
+			text = "Note: Indicator will be dismissed when re-equipping via the addon, as well as when manually re-equipping. It will not, however, appear if you unequip manually."
+		}
+    }
+
+    LAM2:RegisterOptionControls("Disarm", optionsData)
 end
 
 -- MAIN FUNCTIONS ---------------------------------------------------------------------------------
